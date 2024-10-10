1a79fe7b8eba75f41e84b4135db7ceb54ba79f86
Johnny <125989975+KenZohn@users.noreply.github.com>
2024-10-10 16:47:47 -0300

    Feito

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("\nSmarthphone Nokia:");
+Nokia nokia = new Nokia("1234-5678", "NK4S", "2389247937", 128);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Duolingo");
+
+Console.WriteLine("\nSmarthphone iPhone:");
+Iphone iphone = new Iphone("2222-4444", "Iphone 50 Max", "9209848119", 64);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Tiktok");
